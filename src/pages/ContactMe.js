eb4ee25813a9d392780e6f08682eb5981b93961b
saba rahman <sabarahman.official@gmail.com>
2022-06-09 23:10:21 +0600

    first deploy

--- src/pages/ContactMe.js
+++ src/pages/ContactMe.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import JotformEmbed from 'react-jotform-embed';
+import JotFormReact from 'jotform-react';
 
 const ContactMe = () => {
     return (
         <div>
             <h1>This is contact</h1>
             {/* <script type="text/javascript" src="https://form.jotform.com/jsform/221594277623461"></script> */}
-            <JotformEmbed src="https://form.jotform.com/jsform/221594277623461" />
+            <JotFormReact
+                formURL="https://form.jotform.com/221594277623461"
+            />
         </div>
     );
 };
